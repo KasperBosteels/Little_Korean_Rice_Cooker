fffe391bb48b1fa5ee2bf6cc174947c9b13b106d
kasper1998 <kasperbosteels2@gmail.com>
2020-08-09 23:28:43 +0200

    message test 30001

--- bot.js
+++ bot.js
@@ -7,20 +7,33 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-    client.on('msg', msg => {
-        if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+    client.on('message', msg => {
+        if (!Message.content.startsWith(prefix) || Message.author.bot) return;
 
-        const args = msg.content.slice(prefix.length).trim().split(' ');
+        const args = Message.content.slice(prefix.length).trim().split(' ');
         const command = args.shift().toLowerCase();
 
          if (command === 'args-info') {
             if (!args.length) {
-                return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
+                return Message.channel.send(`You didn't provide any arguments, ${Message.author}!`);
             }
         
-            msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
+            Message.channel.send(`Command name: ${command}\nArguments: ${args}`);
         }
     
+    });
+
+
+
+
+//initiate bot by connecting to server
+client.login(process.env.DISCORD_TOKEN);
+
+
+
+
+
+/*
 switch (msg.content){
     case    `${config.prefix}meow`:
         msg.reply('pspspspspspspspspsp')
@@ -34,17 +47,4 @@ switch (msg.content){
     case `${config.prefix}server`:
         msg.reply(`server name: ${Discord.guild.name}\nTotal members: ${Discord.Guild.length}`);
 };
-    });
-
-
-
-
-//initiate bot by connecting to server
-client.login(process.env.DISCORD_TOKEN);
-
-
-
-
-
-
- 
+ */
