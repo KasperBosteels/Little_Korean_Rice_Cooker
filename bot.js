a0d3032743b387d3ebb7a91f60654109cdb29e23
K. Bosteels <55544564+kasper1998@users.noreply.github.com>
2020-08-09 20:22:58 +0200

    first
    adding base files

---
+++ bot.js
@@ -0,0 +1,46 @@
+// Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('message', msg => {
+    if (msg.content ==='ping'){msg.reply('pong');
+}
+});
+
+
+
+
+//initiate bot by connecting to server
+client.login(process.env.DISCORD_TOKEN);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
