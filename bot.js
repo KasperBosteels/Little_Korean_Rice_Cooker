d1b466c451790ec599bf95bde0c94f9487ff21a5
kasper1998 <kasperbosteels2@gmail.com>
2020-08-10 01:05:09 +0200

    avatar

--- bot.js
+++ bot.js
@@ -103,6 +103,22 @@ client.on('ready', () => {
                 }
 
 
+
+
+                else if (command === 'avatar') {
+                    if (!message.mentions.users.size) {
+                        return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+                    }
+                
+                    const avatarList = message.mentions.users.map(user => {
+                        return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+                    });
+                
+                    // send the entire array of strings as a message
+                    // by default, discord.js will `.join()` the array with `\n`
+                    message.channel.send(avatarList);
+                }
+
             
     });
 
