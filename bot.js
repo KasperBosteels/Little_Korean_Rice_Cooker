0075e61cf462a9ca6b254944793cfd15679e90b6
kasper1998 <kasperbosteels2@gmail.com>
2020-08-10 00:03:13 +0200

    coin trow test

--- bot.js
+++ bot.js
@@ -3,14 +3,14 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const config = require('./auth.json');
 const client = new Discord.Client();
-
+let coin;
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
     client.on('message', message => {
         if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-        const args = message.content.slice(config.prefix.length).trim().split(' ');
+        const args = message.content.slice(config.prefix.length).trim().split(/ +/);
         const command = args.shift().toLowerCase();
         if (command === 'args-info') {
             if (!args.length) {
@@ -18,6 +18,28 @@ client.on('ready', () => {
             }
         
             message.channel.send(`Command name: ${command}\nArguments: ${args}`);}
+
+
+            else if (command === 'args-info') {
+                if (!args.length) {
+                    return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+                }
+                else if (args[0] === 'foo') {
+                    return message.channel.send('bar');
+                }
+            
+                message.channel.send(`First argument: ${args[0]}`);
+            }
+            else if (command === 'coin') {
+                coin = Math.floor((Math.random() * 2) + 1);
+                if (coin <= 1){coin = 'tails';}else coin >= 'heads';
+                return message.channel.send(`${coin}`);
+                }
+            
+                
+            
+
+            
     });
 
 
@@ -27,7 +49,17 @@ client.on('ready', () => {
 client.login(process.env.DISCORD_TOKEN);
 
 
+/*
+else if (command === 'args-info') {
+	if (!args.length) {
+		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+	}
+	else if (args[0] === 'foo') {
+		return message.channel.send('bar');
+	}
 
+	message.channel.send(`First argument: ${args[0]}`);
+*/
 
 
 /*
