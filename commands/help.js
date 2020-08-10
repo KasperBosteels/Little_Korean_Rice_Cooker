e6e5508cc9193cea1377e466a2bd55a60ce276a2
kasper1998 <kasperbosteels2@gmail.com>
2020-08-10 13:45:22 +0200

    retest help

--- commands/help.js
+++ commands/help.js
@@ -10,27 +10,7 @@ module.exports = {
 const { commands } = message.client;
 
     if (!args.length) {
-        const name = args[0].toLowerCase();
-        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-if (!command) {
-	return message.reply('that\'s not a valid command!');
-}
-
-data.push(`**Name:** ${command.name}`);
-
-if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-if (command.description) data.push(`**Description:** ${command.description}`);
-if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-
-message.channel.send(data, { split: true });
-
-
-
-
-
+        
         data.push('Here\'s a list of all my commands:');
         data.push(commands.map(command => command.name).join(', '));
         data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
@@ -45,7 +25,22 @@ message.channel.send(data, { split: true });
                 message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
             });
     }
-
+    const name = args[0].toLowerCase();
+    const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+    
+    if (!command) {
+        return message.reply('that\'s not a valid command!');
+    }
+    
+    data.push(`**Name:** ${command.name}`);
+    
+    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+    if (command.description) data.push(`**Description:** ${command.description}`);
+    if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+    
+    data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+    
+    message.channel.send(data, { split: true });
 
 
 	},
