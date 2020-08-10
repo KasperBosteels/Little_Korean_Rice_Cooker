5d43b097f85641181147ac79b8bb6f15ea35c59e
kasper1998 <kasperbosteels2@gmail.com>
2020-08-10 13:53:46 +0200

    re test help

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
 	cooldown: 5,
 	execute(message, args) {
 		const data = [];
-const { commands } = message.client;
+        const { commands } = message.client;
 
     if (!args.length) {
         
@@ -24,23 +24,25 @@ const { commands } = message.client;
                 console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
                 message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
             });
+    } else {
+        const name = args[0].toLowerCase();
+        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+
+if (!command) {
+	return message.reply('that\'s not a valid command!');
+}
+
+data.push(`**Name:** ${command.name}`);
+
+if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+if (command.description) data.push(`**Description:** ${command.description}`);
+if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
+data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+
+message.channel.send(data, { split: true });
     }
-    const name = args[0].toLowerCase();
-    const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-    
-    if (!command) {
-        return message.reply('that\'s not a valid command!');
-    }
-    
-    data.push(`**Name:** ${command.name}`);
-    
-    if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-    if (command.description) data.push(`**Description:** ${command.description}`);
-    if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-    
-    data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-    
-    message.channel.send(data, { split: true });
+
 
 
 	},
