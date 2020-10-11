7dbbcc8cc595871144eb5320d7441c4e8aba18f7
kasper1998 <kasperbosteels2@gmail.com>
2020-10-11 17:03:31 +0200

    added links command and debug

---
+++ commands/link.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'links',
+	description: 'gives you some handy links in your dm\'s',
+	cooldown: 1,
+	usage: ' ',
+	category: "general",
+	execute(client,message, args) {
+		
+        message.author.send(`https://discord.com/api/oauth2/authorize?client_id=742037772503744582&permissions=805694518&scope=bot \n https://matias.ma/nsfw/ \n `)
+	},
+};
