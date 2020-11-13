6386513907df1f14d3ddae73ce302c6aee81af85
kasper1998 <kasperbosteels2@gmail.com>
2020-11-13 02:44:00 +0100

    info added currency explanations

--- commands/info.js
+++ commands/info.js
@@ -3,6 +3,7 @@ const discord = require('discord.js');
 const info = require('../package.json');
 const database = require("../database.json");
 const mysql = require("mysql");
+const prefix = require('../auth.json');
 module.exports = {
 	name: 'info',
 	description: 'gives general info about the bot',
@@ -27,7 +28,8 @@ module.exports = {
         .setDescription(`version: ${info.version}`)
         .setColor(`#228B22`)
         .addField(`author:`,`${info.author}`)
-        .addField('contact',`have any cool gifs or you would like to send me a message ?\n pls use "-message <your message>".`)
+        .addField('Contact',`Hhave any cool gifs or you would like to send me a message ?\n pls use "-message <your message>".`)
+        .addField('Currency',`Currency is available for level 5 users, use <${prefix.prefix}level> to see your current level.\nIf any problems occur use <${prefix.prefix}message (your message)> to notify dev.`)
         .addField(`dependencies `,true)
         .addField(`discordjs`,`${info.dependencies["discord.js"]}`,true)
         .addField(`dotenv`,`${info.dependencies["dotenv"]}`,true)
