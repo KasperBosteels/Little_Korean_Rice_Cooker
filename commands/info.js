6ee9a6d272d5609c549320f01684124af13eb258
kasper1998 <kasperbosteels2@gmail.com>
2020-11-13 02:44:52 +0100

    cleanup

--- commands/info.js
+++ commands/info.js
@@ -30,7 +30,6 @@ module.exports = {
         .addField(`author:`,`${info.author}`)
         .addField('Contact',`Hhave any cool gifs or you would like to send me a message ?\n pls use "-message <your message>".`)
         .addField('Currency',`Currency is available for level 5 users, use <${prefix.prefix}level> to see your current level.\nIf any problems occur use <${prefix.prefix}message (your message)> to notify dev.`)
-        .addField(`dependencies `,true)
         .addField(`discordjs`,`${info.dependencies["discord.js"]}`,true)
         .addField(`dotenv`,`${info.dependencies["dotenv"]}`,true)
         .addField(`ms`,`${info.dependencies["ms"]}`,true)
