47b89bfad43400150c879cb1bcbc1888f52c6442
kasper1998 <kasperbosteels2@gmail.com>
2020-11-13 02:48:13 +0100

    cleanup

--- commands/info.js
+++ commands/info.js
@@ -28,15 +28,13 @@ module.exports = {
         .setDescription(`version: ${info.version}`)
         .setColor(`#228B22`)
         .addField(`author:`,`${info.author}`)
-        .addField('Contact',`Hhave any cool gifs or you would like to send me a message ?\n pls use "-message <your message>".`)
-        .addField('Currency',`Currency is available for level 5 users, use <${prefix.prefix}level> to see your current level.\nIf any problems occur use <${prefix.prefix}message (your message)> to notify dev.`)
+        .addField('Contact',`Have any cool gifs ?,found a bug ?\n pls use "${prefix.prefix}message (your message)".`)
+        .addField('Currency',`Currency is available for level 5 users, use "${prefix.prefix}level" to see your current level.`)
         .addField(`discordjs`,`${info.dependencies["discord.js"]}`,true)
         .addField(`dotenv`,`${info.dependencies["dotenv"]}`,true)
         .addField(`ms`,`${info.dependencies["ms"]}`,true)
         .addField(`mysql`,`${info.dependencies["mysql"]}`,true)
-        .addField(`sql connection:`,`${connection}`,true)
-        .addField(`winston`,`${info.dependencies["winston"]}`,true)
-        
+        .addField(`sql connection:`,`${connection}`,true)        
         //send embeded message
         return message.channel.send(messageembed);
         });
