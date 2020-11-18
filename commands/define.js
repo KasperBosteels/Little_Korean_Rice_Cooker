53cdff7a8d9df6c9cf9ddefd7a12a4e407d1d1aa
kasper1998 <kasperbosteels2@gmail.com>
2020-11-18 17:22:21 +0100

    added dictionary command

---
+++ commands/define.js
@@ -0,0 +1,44 @@
+const ud = require('urban-dictionary');
+const discord = require('discord.js');
+module.exports = {
+	name: 'define',
+	description: 'define a word',
+	cooldown: 1,
+	usage: ' ',
+	category: "fun",
+	 execute(client,message, args,con) {
+		let word =args[0]
+        let defenition = args[1];
+       if(word){
+       return term(word,message.channel);
+       
+       }
+
+	},
+};
+    function term(word,channel){
+        let explain = ["undefined","undefined"];
+    ud.term(word).then((result)=>{
+       const entries = result.entries
+        explain[0] =entries[0].definition
+        explain[1] = entries[0].example
+        sendembed(makeEmbed(word,explain[0],explain[1],discord),channel);
+   }).catch((error)=>{
+       console.error(error.message);
+       if(error.code = 'ERR_WORD_UNDEFINED'){channel.send('not in the dictionary...sorry');}
+   });
+
+    
+}
+  function makeEmbed(word,def,example,discord){
+    var embed = new discord.MessageEmbed();
+    embed.setTitle(`definition for: ${word}`);
+    embed.setDescription(def);
+    embed.addField('example',example);
+    embed.setAuthor("Urban Dictionary");
+    return embed;
+
+}
+function sendembed(embed,channel){
+    return channel.send(embed);
+}
