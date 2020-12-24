381d683e856318a73714e7bb6b104b925432ee40
KasperBosteels <kasperbosteels2@gmail.com>
2020-12-24 13:56:10 +0100

    debug in member join/leave with sqlserverconnectio

--- sql_serverconnection.js
+++ sql_serverconnection.js
@@ -34,18 +34,10 @@ function sqlconnect(con) {
 function embedlog(con,member,embed){
     //#region looks for bot-log channel
     
-        
-con.query(`SELECT EXISTS(SELECT * FROM logchannel WHERE guildID = "${member.guild.id}")AS exist;`,(err,rows) =>{
-    var logchannel;
-    if(err)console.log(err);
-    if(rows[0].exist != 0){
-        con.query(`SELECT channelID AS channel FROM logchannel WHERE guildID = '${member.guild.id}';`,(err,rows) =>{
-            if(err) return console.log(err);
-            logchannel = member.guild.channels.cache.get(rows[0].channel);
-            logchannel.send(embed); 
-  
-        });
-    }else{
+        var logchannel =searchLogData(con,member);
+        if(logchannel){
+        logchannel.send(embed);
+        }else{
         var lognames = ["bot-logs","bot-log","log","botllog"];
         for (let u = 0; u < lognames.length; u++) {
              logchannel = member.guild.channels.cache.find(chan => chan.name === lognames[u]);
@@ -57,21 +49,12 @@ con.query(`SELECT EXISTS(SELECT * FROM logchannel WHERE guildID = "${member.guil
         if (!logchannel) {console.log('no action taken no channel found');
       }else{  logchannel.send(embed); 
       }}
-    });
     //endconnect(con);
 }
 function embedless(con,member,embed,message){
-       
-con.query(`SELECT EXISTS(SELECT * FROM logchannel WHERE guildID = "${member.guild.id}")AS exist;`,(err,rows) =>{
-    var logchannel;
-    if(err)console.log(err);
-    if(rows[0].exist != 0){
-        con.query(`SELECT channelID AS channel FROM logchannel WHERE guildID = '${member.guild.id}';`,(err,rows) =>{
-            if(err) return console.log(err);
-            logchannel = member.guild.channels.cache.get(rows[0].channel);
-            logchannel.send(embed); 
-  
-        });
+ var logchannel = searchLogData(con,member);
+ if(logchannel){
+     logchannel.send(embed)
     }else{
         var lognames = ["bot-logs","bot-log","log","botlog"];
         for (let u = 0; u < lognames.length; u++) {
@@ -84,7 +67,6 @@ con.query(`SELECT EXISTS(SELECT * FROM logchannel WHERE guildID = "${member.guil
         if (!logchannel) {message.channel.send(embed);
       }else{  logchannel.send(embed); 
       }}
-    });
 }
 //get log channel and send welcome or leave message
 function logget(con,member,happening) {
@@ -124,6 +106,24 @@ function logget(con,member,happening) {
     }}
         });
 }
-function endconnect(con){
+function endconnect(con, member){
     con.end(err =>{if (err)return console.log(err);});
 }
+function searchLogData(con,member){
+
+    con.query(`SELECT EXISTS(SELECT * FROM logchannel WHERE guildID = "${member.guild.id}")AS exist;`,(err,rows) =>{
+        if(err)return console.log(err);
+        if(rows[0].exist != 0){
+            con.query(`SELECT channelID AS channel FROM logchannel WHERE guildID = '${member.guild.id}';`,(err,rows) =>{
+                if(err) return console.log(err);
+                return member.guild.channels.cache.get(rows[0].channel);
+                 
+      
+            });
+        }
+        return false;
+    });
+
+
+
+    }
