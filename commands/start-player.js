48d5329966e6c1c5c9f16f13fb5f7f6e354168a6
KasperBosteels <kasperbosteels2@gmail.com>
2021-02-03 16:31:01 +0100

    better audio

--- commands/start-player.js
+++ commands/start-player.js
@@ -1,5 +1,5 @@
 const ytdl = require('ytdl-core');
-const ytdlD = require('ytdl-core-discord');
+//const ytdl = require('ytdl-core-discord');
 module.exports = {
 	name: 'sing',
 	description: 'I will sing you a song',
@@ -12,7 +12,7 @@ module.exports = {
         if(!args[0])return message.channel.send('you need to give me a song url.');
         let validate = await ytdl.validateURL(args[0]);
         if(!validate)return message.channel.send('url not found sorry.');
-        let info = await ytdl.getInfo(args[0]);
+        let info = await  ytdl.getInfo(args[0]);
         let data = options.active.get(message.guild.id) || {};
 
         if(!data.connection)data.connection = await message.member.voice.channel.join();
@@ -24,11 +24,7 @@ module.exports = {
             url: args[0],
             announceChannel: message.channel.id
         });
-        for (let i = 0; i < data.queue.length; i++) {
-            console.log(data.queue[i].songTitle);      
-        }
         if(!data.dispatcher){
-            console.log(info.videoDetails.title);
             Play(client,options,data);
         }else{
             message.channel.send(`added to the queue: ${info.videoDetails.title} | requested by: ${message.author.tag}`);
@@ -39,7 +35,7 @@ module.exports = {
 };
 async function Play(client,o,data){
 client.channels.cache.get(data.queue[0].announceChannel).send(`PLAYING: ${data.queue[0].songTitle} - requested by: ${data.queue[0].requester}`);
-let options = {seek: 3,volume: 0.7,bitrate: 128000,quality: 'highestaudio',highWaterMark: 100};
+let options = {seek: 3,volume: 0.7,bitrate: 128000,quality: 'highestaudio',highWaterMark: 300};
 data.dispatcher = await data.connection.play(ytdl(data.queue[0].url,{filter: 'audioonly'}), options);
 data.dispatcher.guildID = data.guildID;
 data.dispatcher.once('finish',function () {
