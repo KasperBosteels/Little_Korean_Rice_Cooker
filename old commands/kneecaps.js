4cffe57f9912f353f550e2977adf5d8b4b7149f8
KasperBosteels <kasperbosteels2@gmail.com>
2021-03-02 20:59:10 +0100

    debugging and kill added, kneecaps in progress

---
+++ old commands/kneecaps.js
@@ -0,0 +1,12 @@
+const kneecaps = ["https://i.imgur.com/Ms1tM1A.jpg","https://i.pinimg.com/736x/e9/a0/38/e9a038a793dbdba88c0a4075ca45abc7.jpg","https://i.imgflip.com/3fah49.jpg","https://img-9gag-fun.9cache.com/photo/aADY5oE_700bwp.webp",
+]
+module.exports = {
+	name: 'kneecaps',
+	description: 'when someone loses their kneecap certificate ur allowed to take them.',
+	cooldown: 1,
+	usage: '<@user>',
+	category: "fun",
+	execute(client,message, args,con) {
+
+    },
+};
