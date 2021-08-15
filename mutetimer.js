3e923af54b6c6b2c8612d272709a1ead6dff4045
kasperBosteels <kasperbosteels2@gmail.com>
2021-08-16 00:17:23 +0200

    QOS

--- mutetimer.js
+++ mutetimer.js
@@ -3,7 +3,7 @@ const ms = require('ms');
 module.exports = {
     async execute(time,member,role,message){
         if (!time) return console.log('Not able to determine mutetime.');
-        if (member.hasPermission("KICK_MEMBERS")) return
+        if (member.hasPermission("KICK_MEMBERS")) return message.reply('This person is possibly a moderator and cannot be muted.');
 
         //adds mute role to user
         member.roles.add(role.id);
