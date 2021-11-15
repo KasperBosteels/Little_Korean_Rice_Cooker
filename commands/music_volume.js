7e3d6f3f6d8fc47a4428dfe88afae4ac85c20085
kasperBosteels <kasperbosteels2@gmail.com>
2021-11-15 23:49:48 +0100

    setting up more stuff and debugging volume again

--- commands/music_volume.js
+++ commands/music_volume.js
@@ -18,3 +18,49 @@ module.exports = {
     score.ADD(con, 1, message.author.id);
   },
 };
+/*
+
+exports.volume = async (options = {}) => {
+
+    const { interaction, volume } = options;
+    if(!interaction) throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
+    if(!volume || !Number.isInteger(volume) || volume > 100) throw new Error(`INVALID_VOLUME: There is no valid Volume Integer provided or the number is higher than 100.`);
+    if(!activeSongs.has(interaction.guild.id) || !activeSongs.get(interaction.guild.id)?.connection || !activeSongs.get(interaction.guild.id)?.player) throw new Error(`NO_MUSIC: There is no music playing in that server.`);
+
+    const fetchedData = await activeSongs.get(interaction.guild.id);
+
+    fetchedData.resource.volume.setVolumeLogarithmic(volume/1000)
+
+};
+
+async function playSong(data, interaction) {
+
+    let resource = await createAudioResource(ytdl(data.queue[0].url, { filter: 'audioonly',highWaterMark: 1<<25, quality:'highestaudio'}), { 
+        inputType: StreamType.Arbitrary,
+        inlineVolume: true
+    });
+
+    const player = createAudioPlayer();
+    player.play(resource);
+    data.player = player;
+    data.resource = resource
+    data.dispatcher = await data.connection.subscribe(player);
+    data.dispatcher.guildId = data.guildId;
+    resource.volume.setVolumeLogarithmic(1/10);
+    if(data.queue[0].info.extra.type === 'playlist') {
+        event.emit('playList', data.queue[0].channel, data.queue[0].info.extra.playlist, data.queue[0].info, data.queue[0].requester);
+    } else {
+        event.emit('playSong', data.queue[0].channel, data.queue[0].info, data.queue[0].requester);
+    }
+
+    player.on(AudioPlayerStatus.Idle, async () => {
+
+        finishedSong(player, data.connection, data.dispatcher, interaction);
+
+    })
+
+    player.on('error', err => console.log(err))
+
+};
+
+*/
