5a37599ea02ef931b898fa57e8ce5f8242ed0b31
kasperBosteels <kasperbosteels2@gmail.com>
2022-02-14 19:10:06 +0100

    get queue improved

--- commands/music_getqueue.js
+++ commands/music_getqueue.js
@@ -11,14 +11,13 @@ module.exports = {
   userperms: ["CONNECT"],
   async execute(client, message, args, con) {
     let list = await music.getQueue({ interaction: message });
-    console.log(list);
     let embed = new Discord.MessageEmbed()
       .setTitle("QUEUE")
       .setDescription("The queue for this PARTAY!")
       .setColor("RANDOM");
     for (let i = 0; i < list.length; i++) {
       embed.addField(
-        `${list[i].info.title}`,
+        `[${i + 1}]${list[i].info.title}`,
         `duration: ${list[i].info.duration}  author: ${list[i].info.author} requested by:${list[i].requester}`
       );
     }
