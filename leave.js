1487482e169283d8730a9f618551138c0995c407
kasperBosteels <kasperbosteels2@gmail.com>
2022-04-29 14:17:52 +0200

    reconfig leave

--- leave.js
+++ leave.js
@@ -1,7 +1,7 @@
 module.exports = {
   execute(message, client) {
     if (message.author.id != "258217948819357697") return;
-    if (message.content.includes("*leave")) {
+    if (message.content.includes("*sayonara")) {
       message.channel
         .send({ content: "Sayonara" })
         .then(() => {
