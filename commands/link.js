ab03cf71d2917dee90cd420256cbb8eca9b25263
kasperBosteels <kasperbosteels2@gmail.com>
2022-07-03 19:26:11 +0200

    fixed links and emojis

--- commands/link.js
+++ commands/link.js
@@ -1,3 +1,4 @@
+const G = require("../Generators/GenerateSimpleEmbed").GenerateEmbed;
 module.exports = {
   name: "invite-me",
   description: "Gives you a link to invite the bot to a different server.",
@@ -9,8 +10,25 @@ module.exports = {
   userperms: [],
   execute(client, message, args) {
     message.channel.send({
-      content: `https://canary.discord.com/api/oauth2/authorize?client_id=742037772503744582&permissions=1514516376646&scope=applications.commands%20bot <:cooker:927889501295095879>`,
+      content: "<:cooker:927889501295095879> ",
+      embeds: [
+        G(
+          "#3ac941",
+          "I will do my best for you.",
+          false,
+          false,
+          false,
+          false,
+          false,
+          "https://canary.discord.com/api/oauth2/authorize?client_id=742037772503744582&permissions=1514516376646&scope=applications.commands%20bot"
+        ),
+      ],
     });
+    /*
+    message.channel.send({
+      content: `https://canary.discord.com/api/oauth2/authorize?client_id=742037772503744582&permissions=1514516376646&scope=applications.commands%20bot `,
+    });
+    */
   },
 };
 //latest https://canary.discord.com/api/oauth2/authorize?client_id=742037772503744582&permissions=1514516376646&scope=bot
