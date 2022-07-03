a7306cb031f3d0e882c5531b83a7a95cfa8a4eaf
Bosteels Kasper (s106631) <kasper.bosteels@student.ap.be>
2022-07-03 04:10:37 +0200

    added lie detector and improvement

--- text responses/rice.js
+++ text responses/rice.js
@@ -2,18 +2,23 @@ const fs = require("fs");
 module.exports = {
   async execute(message) {
     let reply = async (words) => await message.channel.send({ content: words });
-    const swear = await getswearwords();
-    const messageArray = message.content.split().toLowerCase();
-    let messageContent = response(swear, messageArray);
-    if (messageContent != false) return reply(messageContent);
+    let tempstring = message.content.toLowerCase();
+    if (array.length == 1 && array[0].toLowerCase() == "meow") {
+      return reply("pspspspspspspspsps, here kitty.");
+    } else if (tempstring == "bot lies" || tempstring == "bot lie") {
+      return reply("I don't lie!");
+    } else {
+      const messageArray = message.content.split().toLowerCase();
+      const swear = await getswearwords();
+      let messageContent = response(swear, messageArray);
+      if (messageContent != false) return reply(messageContent);
+    }
   },
 };
 function getswearwords() {
   return JSON.parse(fs.readFileSync("./jsonFiles/swearwords.json"));
 }
 function response(swear, array) {
-  if (array.length == 1 && array[0].toLowerCase() == "meow")
-    return "pspspspspspspspsps, here kity.";
   for (let A = 0; A < array.length; A++) {
     const userWord = array[A].toLowerCase();
     swear["good"].map((word) => {
