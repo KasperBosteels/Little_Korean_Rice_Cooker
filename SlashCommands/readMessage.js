71d68f844b2abac2f93bba1bf929e400d533576d
kasperBosteels <kasperbosteels2@gmail.com>
2022-07-17 19:04:47 +0200

    added read message slash SlashCommands

---
+++ SlashCommands/readMessage.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const G = require("../Generators/GenerateSimpleEmbed").GenerateEmbed;
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("read_mail")
+    .setDescription("Read Your mail")
+    .setDefaultPermission(true),
+  async execute(client, interaction, con) {
+    await con.query(
+      `SELECT * FROM user_messages WHERE userID = ${interaction.member.id}`,
+      (err, rows) => {
+        if (err) return console.error(err);
+        let embed = G("#000ff", "all your messages", false, false, true);
+        rows.forEach((row) => {
+          embed.addField(`${row.topic}`, `${row.message}`, false);
+          if (row.Reply != undefined) {
+            embed.addField("reply:", `\`\`\`${row.Reply}\`\`\``, true);
+          } else {
+            embed.addField("reply:", `no reply`, true);
+          }
+        });
+        interaction.reply({ embeds: [embed], ephemeral: true });
+      }
+    );
+  },
+};
