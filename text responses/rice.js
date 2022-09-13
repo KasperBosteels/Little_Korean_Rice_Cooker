343ec9b5b5fd03015f8763c3bd60d88735b5c284
kasperBosteels <kasperbosteels2@gmail.com>
2022-09-13 20:01:18 +0200

    changes

--- text responses/rice.js
+++ text responses/rice.js
@@ -13,44 +13,16 @@ module.exports = {
       reply("I don't lie!");
       return;
     } else {
-<<<<<<< HEAD
-      let messageContent = await response(
-        getswearwords(),
-        tempstring.split(" ")
-      );
-      if (messageContent === false) {
-        console.log(tempstring, messageContent);
-        return;
-      } else {
-        reply(messageContent);
-        return;
-      }
-=======
       let swears = await getswearwords();
       await response(swears, tempstring).then((word) => {
         if (word != false) return reply(word);
       });
->>>>>>> 9f7e88a0f2588f101e24e3f0bc495d57c4d9aebd
     }
   },
 };
 function getswearwords() {
   return JSON.parse(fs.readFileSync("./jsonFiles/swearwords.json"));
 }
-<<<<<<< HEAD
-async function response(swear, array) {
-  let cache;
-  for (let A = 0; A < array.length; A++) {
-    cache = array[A].toLowerCase();
-    swear["good"].map((word) => {
-      if (cache.includes(word)) return "thank you (っ ᵔ ₃ ᵔ )っ🎔";
-    });
-    swear["rice"].map((word) => {
-      if (cache.includes(word)) return "Here you go, :rice:";
-    });
-    swear["bad"].map((word) => {
-      if (cache.includes(word)) return "Yeah, you're rubbing off on me.";
-=======
 async function response(swear, input) {
   let returnValue = false;
   swear["good"].forEach((word) => {
@@ -64,7 +36,6 @@ async function response(swear, input) {
   if (returnValue === false) {
     swear["bad"].forEach((word) => {
       if (input.includes(word)) returnValue = "Yeah, you're rubbing off on me.";
->>>>>>> 9f7e88a0f2588f101e24e3f0bc495d57c4d9aebd
     });
   }
   return returnValue;
