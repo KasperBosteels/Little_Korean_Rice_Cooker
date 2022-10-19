3c163ef79938cd6865c194fa105e4d06c7d8ece0
kasperBosteels <kasperbosteels2@gmail.com>
2022-10-19 21:18:37 +0200

    removed unused code

--- commands/SlashCommands/help.js
+++ commands/SlashCommands/help.js
@@ -6,28 +6,7 @@ module.exports = {
     .setName("help")
     .setDescription("A handy guide for the bot.")
     .setDefaultPermission(true),
-  /*    
-    .addStringOption((option) =>
-      option
-        .setName("category")
-        .setDescription("Select a specific help category.")
-        .setRequired(false)
-        .addChoices(
-          { name: "Index", value: "1" },
-          { name: "General", value: "2" },
-          { name: "Fun", value: "3" },
-          { name: "Music", value: "4" },
-          { name: "Moderating", value: "5" },
-          { name: "Configuration", value: "6" }
-        )
-    ),
-    */
   async execute(client, interaction, con) {
-    /*const cat = interaction.options.getString("category");
-    if (cat) {
-      await help.execute(client, interaction, [cat], null, null, null);
-    }
-    */
     await help.execute(client, interaction, [], null, null, null);
   },
 };
