02fb85220bb068518cbc73f6ec690a749db1989c
kasperBosteels <kasperbosteels2@gmail.com>
2022-10-23 14:29:37 +0200

    updated REST version

--- uploadSlashCommand.js
+++ uploadSlashCommand.js
@@ -2,7 +2,7 @@ const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9"); //#endregion
 module.exports = {
   async execute(slashcommands, token) {
-    const rest = new REST({ version: "9" }).setToken(token);
+    const rest = new REST({ version: 10 }).setToken(token);
     const guildid = "927741496738869298";
     const clientId = "742037772503744582";
     (async () => {
