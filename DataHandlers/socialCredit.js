9c88161fa24ac206719889b0cb7e2d8bc7888350
Kasper B <kasperbosteels@hotmail.com>
2023-01-04 01:30:53 +0100

    fixed a bug

--- DataHandlers/socialCredit.js
+++ DataHandlers/socialCredit.js
@@ -2,16 +2,22 @@
 module.exports = {
   async ADD(con, score = 1, userID) {
     let m = await con.manager.findOneBy("User",{user_id:userID})
-    m.user_score+=score
+   const points = parseInt(m.user_score)+score
+   m.user_score=points
     await con.manager.save("Users",m)
   },
   async SUBTRACT(con, score = 1, userID) {
     const m = await con.manager.findOneBy("User",{user_id:userID})
-    m.user_score -= score
+    const points = parseInt(m.user_score) - score
+    m.user_score=points;
     con.manager.save("Users",m)
   },
   async GETSCORE(con, userID) {
     const m = await con.manager.findOneBy("User",{user_id:userID})
-    return m.user_score? m.user_score : 1000;
+    const score = parseInt(m.user_score)
+    return score? score : 1000;
   },
+  async ADDUSER(con,user){
+    await con.manager.insert("Users",{user_id:user.id,user_name:user.displayName,user_score:1000})
+  }
 };
