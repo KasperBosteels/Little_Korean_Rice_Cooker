35ae2a6f208627e3662f645b9d75889e151a17b9
Kasper B <kasperbosteels@hotmail.com>
2023-01-24 17:32:05 +0100

    refactored code to work with orm

--- commands/Custom_welcome_message.js
+++ commands/Custom_welcome_message.js
@@ -11,79 +11,52 @@ module.exports = {
   category: "config",
   perms: ["SendMessages"],
   userperms: ["Administrator"],
-  execute(client, message, args, con) {
+  async execute(client, message, args, con) {
     //check perms
     if (!permission(message))
       return message.reply({
         content: "Only an administrator is able to use this command.",
       });
     //assigns id to variables and check if received
-    var guild = message.guild.id;
-    if (!guild)
+    var guildid = message.guild.id;
+    if (!guildid)
       return console.log(
         "I was unnable to determine this guilds id, try again later."
       );
-
+      const guild = await con.manager.findOneBy('Guilds',{guild_id:guildid});
     if (args[0] && args[0].toLowerCase() == "disable") {
-      con.query(
-        `SELECT EXISTS(SELECT welcome_message FROM custom_welcome_message WHERE guildID = "${guild}")AS exist;`,
-        (err, rows) => {
-          if (err) {
-            console.log(err);
-            message.channel.send("ERROR something broke.");
-          }
-          if (rows[0].exist != 0) {
-            con.query(
-              `UPDATE custom_welcome_message SET welcome_message = NULL WHERE guildID = "${guild}";`
-            );
-            message.channel.send({
-              content: "I removed your servers welcome message.",
-            });
-          } else {
-            return message.channel.send({
-              content: "There was no custom welcome message send.",
-            });
-          }
-        }
-      );
+    
+      guild.welcome_message=undefined;
+      try{
+        await con.manager.save("Guids",guild)
+        await welcome_data.execute(con);
+        return message.channel.send({
+          content: "I removed your servers welcome message.",
+        });
+      }catch(e){
+        console.log(e)
+        message.channel.send({
+          content: "An error occured try again later.",
+        });
+      }
     } else {
       //checks if database already exists if true update else insert
-      con.query(
-        `SELECT EXISTS(SELECT welcome_message FROM custom_welcome_message WHERE guildID = "${guild}")AS exist;`,
-        (err, rows) => {
-          if (err) {
-            console.log(err);
-            message.channel.send({ content: "Something broke, sorry." });
-          }
-          if (rows[0].exist != 0) {
-            con.query(
-              `UPDATE custom_welcome_message SET welcome_message = '${args.join(
-                " "
-              )}' WHERE guildID = '${guild}';`
-            );
-          } else {
-            con.query(
-              `INSERT INTO custom_welcome_message (guildID,welcome_message) VALUES("${guild}","${args.join(
-                " "
-              )}");`,
-              (err) => {
-                if (err) {
-                  console.log(err);
-                  message.channel.send({
-                    content: "Something broke, very sorry.",
-                  });
-                }
-              }
-            );
-          }
-          welcome_data.execute(con);
-          return message.channel.send({
-            content: "Your custom welcome message was saved.",
-          });
-        }
-      );
+      guild.welcome_message = args.join(" ");
+      try{
+        await con.manager.save("Guilds",guild)
+        await welcome_data.execute(con);
+        return message.channel.send({
+          content: "Your custom welcome message was saved.",
+        });
+      }catch(e){
+        console.log(e)
+        message.channel.send({
+          content: "An error occured try again later.",
+        });
+
+      }
     }
-  },
+  }
 };
 function permission(message) {
   //check perms
